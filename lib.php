92a63aecb7b4e294d0c065303a2174c29a8abd10
Ramon de Oliveira Araújo Mateus <ramonmateus00@gmail.com>
2023-06-07 14:27:13 -0300

    tentativa de rodar o moodle events

--- lib.php
+++ lib.php
@@ -24,32 +24,140 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-function course_deleted_observer(\core\event\course_deleted $event) {
-    global $DB;
-    
-    $context = $event->get_context();
-    $courseid = $context->instanceid;
-    
-    $aulas = array_values($DB->get_records_sql('
-    SELECT a.id
-    FROM mdl_suapattendance_aula a 
-        INNER JOIN mdl_course_sections s ON (a.sectionid = s.id) WHERE course = ?;
-    ', [$courseid]));
+// require_once('config.php');
 
-    if ($aulas) {
-        foreach($aulas as $aula) {
-            $DB->delete_records('suapattendance_aula', ['id'=>$aula->id]);
-        }
-    }
-}
+// // Definir o evento de exclusão do curso
+// $event = \core\event\course_deleted::create(array(
+//     'context' => context_system::instance(),
+//     'objectid' => $courseid, // ID do curso que foi apagado
+// ));
 
-// Registrar o manipulador de evento para o evento 'course_deleted'
-$observers = [
-    [
-        'eventname' => '\core\event\course_deleted',
-        'callback' => 'course_deleted_observer',
-    ],
-];
+// // Disparar o evento
+// $event->trigger();
+
+// // Função de manipulador de evento
+// function course_deleted_handler(\core\event\course_deleted $event) {
+//     // Código para executar quando um curso for apagado
+//     // Você pode adicionar ações personalizadas aqui
+//     $courseid = $event->objectid;
+//     echo "O curso com o ID $courseid foi apagado.";
+// }
+
+// // Registrar o observador de evento
+// $observers = array(
+//     array(
+//         'eventname' => '\core\event\course_deleted',
+//         'callback' => 'course_deleted_handler',
+//     ),
+// );
+
+
+
+// class mod_meu_plugin_event_listener {
+
+
+//     protected function init() {
+//         $this->data['objecttable'] = 'course';
+//         $this->data['crud'] = 'r';
+//         // $this->data['edulevel'] = self::LEVEL_PARTICIPATING;
+//     }
+
+//     public static function course_deleted(\core\event\course_deleted $event) {
+//         $data = $event->get_data();
+//         $courseid = $data['objectid'];
+
+//         die();
+//     }
+// }
+
+// $observers = array(
+//     array(
+//         'eventname' => '\core\event\course_deleted',
+//         'callback' => 'mod_meu_plugin_event_listener::course_deleted',
+//     ),
+// );
+
+
+
+// $observers = array(
+//     array(
+//         'eventname' => '\core\event\course_deleted',
+//         'callback' => function (\core\event\course_deleted $event) {
+//             // $data = $event->get_data();
+//             // $courseid = $data['objectid'];
+
+//             die();
+//         }
+//     ),
+// );
+
+
+
+// class mod_meu_plugin_event_listener extends base {
+
+    // protected function init() {
+    //     $this->data['objecttable'] = 'course';
+    //     $this->data['crud'] = 'd';
+    //     $this->data['edulevel'] = self::LEVEL_TEACHING;
+    // }
+
+    // public static function get_name() {
+    //     return 'mod_meu_plugin_event';
+    // }
+
+    // public function get_description() {
+    //     return 'Ouvinte de evento para exclusão de curso no Meu Plugin';
+    // }
+
+    // public function get_url() {
+    //     return new \moodle_url('/mod/meu_plugin/view.php', array('id' => $this->courseid));
+    // }
+
+//     public static function create_from_event(\core\event\base $event) {
+//         $data = $event->get_data();
+//         $courseid = $data['objectid'];
+
+//         $DB->delete_records('suapattendance_aula', ['id'=>16]);
+//     }
+// }
+
+// $observers = array(
+//     array(
+//         'eventname' => '\core\event\course_deleted',
+//         'callback' => 'mod_meu_plugin_event_listener::create_from_event',
+//     ),
+// );
+
+
+
+// function course_deleted_observer(\core\event\course_deleted $event) {
+//     global $DB;
+    
+//     $DB->delete_records('suapattendance_aula', ['id'=>16]);
+
+    // $context = $event->get_context();
+    // $courseid = $context->instanceid;
+    
+
+    // $aulas = array_values($DB->get_records_sql('
+    // SELECT a.id
+    // FROM mdl_suapattendance_aula a 
+    //     INNER JOIN mdl_course_sections s ON (a.sectionid = s.id) WHERE course = ?;
+    // ', [$courseid]));
+
+    // if ($aulas) {
+    //     foreach($aulas as $aula) {
+    //         $DB->delete_records('suapattendance_aula', ['id'=>$aula->id]);
+    //     }
+    // }
+// }
+
+// $observers = [
+//     [
+//         'eventname' => '\core\event\course_deleted',
+//         'callback' => 'course_deleted_observer',
+//     ],
+// ];
 
 // foreach ($handlers as $eventname => $callback) {
 //     $observers = event_observer::instances();
@@ -62,8 +170,7 @@ $observers = [
 // }
 
 
-// Registre os manipuladores de eventos
-foreach ($observers as $observer) {
-    $observers = event_observer::instances();
-    $observers->register($observer['eventname'], $observer['callback']);
-}
+// foreach ($observers as $observer) {
+//     $observers = event_observer::instances();
+//     $observers->register($observer['eventname'], $observer['callback']);
+// }
